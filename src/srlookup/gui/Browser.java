8ce61993c335415f4ed260da90c15b1981d09a00
Vegard Løkken <vegard@loekken.org>
2017-11-02 23:56:34 +0100

    Replace Desktop.getDesktop().browse()
    As a result of bad Linux support I decided to rewrite this to not use
    the Desktop class.
    
    Implemented a custom class "Browser" that executes a system call to open
    a browser. It's supposed to work on Windows, MacOS and Linux, but is yet
    to be tested under anything other than Linux.

---
+++ src/srlookup/gui/Browser.java
@@ -0,0 +1,92 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Vegard Løkken
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package srlookup.gui;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Arrays;
+
+/**
+ * Browser
+ *
+ * @author Vegard Løkken <vegard@loekken.org>
+ */
+class Browser {
+    private static final List<String> LINUX_BROWSERS = Arrays.asList("xdg-open",
+            // Fallbacks
+            "firefox", "mozilla", "netscape", "chrome", "chromium",
+            "opera", "epiphany", "konqueror", "links", "lynx");
+
+    private enum OS {
+        WINDOWS,
+        MAC,
+        LINUX
+    }
+
+    private static OS currentOS() throws Exception {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.indexOf("win") >= 0) {
+            return OS.WINDOWS;
+        } else if (os.indexOf("mac") >= 0) {
+            return OS.MAC;
+        } else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0) {
+            return OS.LINUX;
+        } else {
+            throw new Exception("Unknown OS");
+        }
+    }
+
+    public static void open(String url) {
+        OS os;
+        try {
+            os = currentOS();
+        } catch (Exception e) {
+            System.err.println("Error while opening URL: " + e.getMessage());
+            return;
+        }
+
+        Runtime rt = Runtime.getRuntime();
+
+        try {
+            switch (os) {
+                case WINDOWS:
+                    rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
+                    break;
+                case MAC:
+                    rt.exec("open " + url);
+                    break;
+                case LINUX:
+                    String separator = String.format(" \"%s\" || ", url);
+                    String browsers = String.join(separator, LINUX_BROWSERS);
+                    String cmd = String.format("%s \"%s\"", browsers, url);
+                    rt.exec(new String[] { "sh", "-c", cmd });
+                    break;
+            }
+        } catch (IOException e) {
+            System.err.println("Caught exception while trying to browse url: " +
+                    e.getMessage());
+        }
+    }
+}
--- src/srlookup/gui/GUI.java
+++ src/srlookup/gui/GUI.java
@@ -25,7 +25,6 @@
 package srlookup.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.KeyboardFocusManager;
@@ -34,6 +33,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.util.regex.Matcher;
@@ -53,7 +53,7 @@ import srlookup.core.SRDict;
  * @author Vegard Løkken <vegard@loekken.org>
  */
 public class GUI extends JFrame implements SuggestionsReceiver {
-    private static final String VERSION = "0.1.7";
+    private static final String VERSION = "0.1.8";
 
     private String lastText;
 
@@ -66,7 +66,7 @@ public class GUI extends JFrame implements SuggestionsReceiver {
 
     public GUI() {
         super("SRLookup");
-        
+
         lastText = "";
         apiThread = null;
 
@@ -82,7 +82,7 @@ public class GUI extends JFrame implements SuggestionsReceiver {
     }
 
     private void setupGUI() {
-        
+
         // Frameless with a custom border
         setUndecorated(true);
 
@@ -116,7 +116,7 @@ public class GUI extends JFrame implements SuggestionsReceiver {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyChar() == KeyEvent.VK_ENTER) {
                     int index = suggestionsList.getSelectedIndex();
-                    
+
                     if (index >= 0)
                         onListItemSelected(index);
                 }
@@ -128,7 +128,7 @@ public class GUI extends JFrame implements SuggestionsReceiver {
                 JList list = (JList)evt.getSource();
                 if (evt.getClickCount() == 2) {
                     int index = list.locationToIndex(evt.getPoint());
-                    
+
                     if (index >= 0)
                         onListItemSelected(index);
                 }
@@ -139,11 +139,11 @@ public class GUI extends JFrame implements SuggestionsReceiver {
         String creditsTxt = "SRLookup v%s Copyright © 2015 Vegard Løkken";
         credits = new JLabel(String.format(creditsTxt, VERSION),
                 SwingConstants.CENTER);
-        credits.setFont(new Font(null, Font.ITALIC, 9));        
+        credits.setFont(new Font(null, Font.ITALIC, 9));
         credits.setBorder(input.getBorder());
 
         add(input, BorderLayout.NORTH);
-        
+
         pack();
         setLocationRelativeTo(null);
     }
@@ -160,7 +160,7 @@ public class GUI extends JFrame implements SuggestionsReceiver {
     }
 
     private void onListItemSelected(int index) {
-        
+
         String item = (String)suggestionsList.getModel().getElementAt(index);
 
         // Find word part and dictionary part
@@ -190,7 +190,7 @@ public class GUI extends JFrame implements SuggestionsReceiver {
     }
 
     private void fetchSuggestions(String query) {
-        
+
         // Invalidate last thread
         if (apiThread != null && !apiThread.isDone())
             apiThread.setInvalid(true);
@@ -208,7 +208,7 @@ public class GUI extends JFrame implements SuggestionsReceiver {
             }
 
             suggestionsList.setModel(listModel);
-            
+
             add(scrollPane, BorderLayout.CENTER);
             add(credits, BorderLayout.SOUTH);
             pack();
@@ -220,23 +220,21 @@ public class GUI extends JFrame implements SuggestionsReceiver {
     }
 
     private void browseDefinition(String query, SRDict dict) {
-        if (Desktop.isDesktopSupported()) {
-            Desktop desktop = Desktop.getDesktop();
-
-            if (desktop.isSupported(Desktop.Action.BROWSE)) {
-                try {
-                    String encoded = URLEncoder.encode(query, "UTF-8");
-
-                    URI uri = new URI(APIConstants.DEFINITION_URL +
-                            "?OPP=" + encoded + "&" + dict + "=+");
-
-                    desktop.browse(uri);
-                }
-                catch(Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
+        String encodedQuery;
+        try {
+            encodedQuery = URLEncoder.encode(query, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("Caught exception while encoding query: " +
+                    e.getMessage());
+            return;
         }
+
+        String url = String.format("%s?OPP=%s&%s=+",
+                APIConstants.DEFINITION_URL,
+                encodedQuery,
+                dict);
+
+          Browser.open(url);
     }
 
     @Override
